7893e17870c2f4a2c687495496894faea0418fe6
Malachi <malachi.watkins3535@gmail.com>
2022-06-29 17:00:33 -0500

    WIP

--- Main.py
+++ Main.py
@@ -3,20 +3,29 @@ from tkinter import *
 from tkinter import ttk
 
 Storage_Type = ['Tote', 'Gaylord', 'Other']
+Store_Number = ['224', '118']
 def main():
     def done():
 
         return
     win= Tk()
-    win.title("")
+    win.title("Warehouse Management")
     win.geometry("800x500")
-    MainLable=Label(win, text="System Information", font=("Courier 22 bold"))
+
+    MainLable=Label(win, text="Received Information", font=("Courier 22 bold"))
     MainLable.pack()
-    listerVar = StringVar(win)
-    listerVar.set('Lister Name') # Def Value
-    lister = OptionMenu(win, listerVar, *lister_names)
-    lister.config(bg="#ffffff")
-    lister.place(x=20,y=45)
+
+    storageVar = StringVar(win)
+    storageVar.set('Storage Type') # Def Value
+    storage = OptionMenu(win, storageVar, *Storage_Type)
+    storage.config(bg="#ffffff")
+    storage.pack()   #place(x=20,y=45)
+
+    storeVar = StringVar(win)
+    storeVar.set('Store Number') # Def Value
+    store = OptionMenu(win, storeVar, *Store_Number)
+    store.config(bg="#ffffff")
+    store.pack()   #place(x=20,y=45)
 
     ttk.Button(win, text= "Confirm",width= 20, command= done).pack(side = BOTTOM, pady = 10)
 
