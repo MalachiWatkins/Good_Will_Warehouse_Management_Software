7dab1e3cb18567937c4ea3f12771745e9019f91f
Malachi <malachi.watkins3535@gmail.com>
2022-06-29 17:29:45 -0500

    WIP

--- Main.py
+++ Main.py
@@ -40,25 +40,25 @@ def warehouse_worker():
     storageVar.set('Storage Type') # Def Value
     storage = OptionMenu(win, storageVar, *Storage_Type)
     storage.config(bg="#ffffff")
-    storage.pack()   #place(x=20,y=45)
+    storage.place(x=20,y=45)
 
     storeVar = StringVar(win)
     storeVar.set('Store Number') # Def Value
     store = OptionMenu(win, storeVar, *Store_Number)
     store.config(bg="#ffffff")
-    store.pack()   #place(x=20,y=45)
+    store.place(x=135,y=45)
 
     contentsVar = StringVar(win)
     contentsVar.set('Contents') # Def Value
     contents = OptionMenu(win, contentsVar, *contents_list)
     contents.config(bg="#ffffff")
-    contents.pack()   #place(x=20,y=45)
+    contents.place(x=260,y=45)
 
-    problems=Label(win, text="Problems", font=("Courier 14 bold"))
-    problems.pack()
+    problems=Label(win, text="Problems:", font=("Courier 14 bold"))
+    problems.place(x=360,y=45)
     problemsent= Entry(win, width= 75)
     problemsent.focus_set()
-    problemsent.pack()
+    problemsent.place(x=475,y=50)
 
     # text_box = Text(
     #     win,
