a49bc206c0fd9f7718fbc2cdaa9206caa5c9c8e8
Malachi <malachi.watkins3535@gmail.com>
2022-06-29 17:23:46 -0500

    WIP

--- Main.py
+++ Main.py
@@ -2,8 +2,29 @@ from datetime import datetime
 from tkinter import *
 from tkinter import ttk
 
+worker_post = {
+    'Storage_Type': '',
+    'Date_Received': '',
+    'Store_Number': '',
+    'Contents': '',
+    'Problems': '',
+
+}
+
+processor_post = {
+    'Storage_Type': '',
+    'Date_Received': '',
+    'Date_Processed': '',
+    'Manifest_Number': '',
+    'Store_Number': '',
+    'Contents': '',
+    'Problems': '',
+
+}
+
 Storage_Type = ['Tote', 'Gaylord', 'Other']
 Store_Number = ['224', '118']
+contents_list = ['Jewelry', 'Collectables', 'Books', 'Media']
 def warehouse_worker():
     def done():
 
@@ -26,6 +47,25 @@ def warehouse_worker():
     store = OptionMenu(win, storeVar, *Store_Number)
     store.config(bg="#ffffff")
     store.pack()   #place(x=20,y=45)
+
+    contentsVar = StringVar(win)
+    contentsVar.set('Contents') # Def Value
+    contents = OptionMenu(win, contentsVar, *contents_list)
+    contents.config(bg="#ffffff")
+    contents.pack()   #place(x=20,y=45)
+
+    problems=Label(win, text="Problems", font=("Courier 14 bold"))
+    problems.pack()
+    problemsent= Entry(win, width= 75)
+    problemsent.focus_set()
+    problemsent.pack()
+
+    # text_box = Text(
+    #     win,
+    #     height=4,
+    #     width=20
+    # )
+    # text_box.pack(expand=True)
 
 
     # Button for done
