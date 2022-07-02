89b29732cf426ccd15b5736cb3eb038c4a3b8266
Malachi <malachi.watkins3535@gmail.com>
2022-07-02 14:09:04 -0500

    WIP

--- site.py
+++ site.py
@@ -13,7 +13,11 @@ import pymongo
 
 warehouse_db = cluster["WAREHOUSE_MANAGEMENT"]
 receiverCollection = warehouse_db["receiver"]
+processorCollection = warehouse_db["processor"]
+jewleryCollection = warehouse_db["jewlery"]
+FINISHEDCollection = warehouse_db["FINISHED"]
 
+### Add another route for view all and edit ####
 ##############
 
 #### Flask Stuff #####
@@ -31,14 +35,16 @@ app._static_folder = "/templates"
 #### Global Vars ####
 
 today = date.today()
-date_format = today.strftime("%m/%d/%Y")
+date_format = today.strftime("%Y-%m-%d")
 date_proc_format = today
 ######################
 
-
-def rec_post(post): # Receving Document
+def db_post(post, isJewlery): # Receving Document
     if post['Storage_Type'] != '':
-        receiverCollection.insert_one(post)
+        if isJewlery == True:
+            jewleryCollection.insert_one(post)
+        else:
+            receiverCollection.insert_one(post)
     else:
         # DATA NOT SUBMITTED ROUTE GOES HERE
         null = 'null'
@@ -60,21 +66,24 @@ def rec():
     template = jinja_env.get_template('rec.html')
 
     if request.method == 'POST':
-        QUANTITY = request.form['quant']
-        STORAGE_TYPE = request.form['storage_type']
-        STORE_NUMBER = request.form['store_number']
-        ITEM_CONTENTS = request.form['item_contents']
-        PROBLEM_FORM = request.form['problem_form']
-        receiver_post = {
-            'Quantity': QUANTITY,
-            'Storage_Type': STORAGE_TYPE,
-            'Date_Received': date_format,
-            'Store_Number': STORE_NUMBER,
-            'Contents': ITEM_CONTENTS,
-            'Problems': PROBLEM_FORM,
-            }
-        rec_post(post=receiver_post)
-        return template.render()
+            QUANTITY = request.form['quant']
+            STORAGE_TYPE = request.form['storage_type']
+            STORE_NUMBER = request.form['store_number']
+            ITEM_CONTENTS = request.form['item_contents']
+            PROBLEM_FORM = request.form['problem_form']
+            data_post = {
+                'Quantity': QUANTITY,
+                'Storage_Type': STORAGE_TYPE,
+                'Date_Received': date_format,
+                'Store_Number': STORE_NUMBER,
+                'Contents': ITEM_CONTENTS,
+                'Problems': PROBLEM_FORM,
+                }
+            if request.form['item_contents'] != 'Jewelry':
+                db_post(post=data_post, isJewlery=False)
+            else:
+                db_post(post=data_post, isJewlery=True)
+
     return template.render()
 
 @app.route('/rec_finished',methods = ['POST', 'GET']) # Reciving finished post
@@ -83,9 +92,13 @@ def rec_finished():
     return template.render()
 
 ######################################################################
-#################### Processor #################################
+#################### Processor #######################################
 ######################################################################
 
+def proc_search():
+
+
+    return
 @app.route('/proc',methods = ['POST', 'GET']) # Reciving finished post
 def proc_main():
     template = jinja_env.get_template('proc_main.html')
@@ -94,12 +107,22 @@ def proc_main():
 @app.route('/proc_data',methods = ['POST', 'GET']) # Reciving finished post
 def proc_data():
     template = jinja_env.get_template('proc_data.html')
+    if request.method == 'POST':
+        date_selected = request.form['date_select']
+        print(date_selected)
+        proc_query = { "Date_Received": date_selected }
+        proc_documents = receiverCollection.find(proc_query)
+        return template.render(data = proc_documents)
     return template.render()
 
 @app.route('/proc_finished',methods = ['POST', 'GET']) # Reciving finished post
 def proc_finished():
     template = jinja_env.get_template('proc_finished.html')
     return template.render()
+
+######################################################################
+#################### Jewelry ########################################
+######################################################################
 
 if __name__ == '__main__':
         app.run(debug=True, host="0.0.0.0", port=80)
